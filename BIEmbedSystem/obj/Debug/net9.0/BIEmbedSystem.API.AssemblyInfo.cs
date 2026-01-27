b48c3e0227142261ac36721ff92e1c05df8986d3
mokumar-ITC <mokumar@itconvergence.com>
2026-01-27 22:44:13 +0530

    Added the V2 updalod service

--- BIEmbedSystem/obj/Debug/net9.0/BIEmbedSystem.API.AssemblyInfo.cs
+++ BIEmbedSystem/obj/Debug/net9.0/BIEmbedSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIEmbedSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a711b1fbbfb281a922b23aa5570e612d31247002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+024b0fd247a57a5325fb737342d49942d4367179")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIEmbedSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIEmbedSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
